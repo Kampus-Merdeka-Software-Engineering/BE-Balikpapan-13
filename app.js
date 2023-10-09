03c3f8898f2d8e416c5e613f9aaf75cfe2aacd02
denyand <denyandrian2002@gmail.com>
2023-10-10 00:08:08 +0700

    update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use("/course", courseRoutes)
 
 //rute untuk registration
 app.use("/registration", registrationRoutes )
-const PORT = process.env.PORT || 30001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log('server is running on port '+ PORT);
 });
